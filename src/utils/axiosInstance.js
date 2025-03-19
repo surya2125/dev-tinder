836692cabf9d56c2023c51db0f1c997cd2cd86c1
Sharadindu Das <sharadindudas774@gmail.com>
2025-03-19 18:10:35 +0530

    fix: Added the baseurl for backend

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "/api",
+    baseURL: location.hostname === "localhost" ? "http://localhost:6565" : "/api",
     withCredentials: true
 });
