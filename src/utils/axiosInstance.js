0e47f6dbbf69dd1529492aed7990b5b2615ffda6
Sharadindu Das <sharadindudas774@gmail.com>
2025-03-25 13:40:34 +0530

    feat: Added env files to the project

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: location.hostname === "localhost" ? "http://localhost:6565/api/v1" : "/api",
+    // baseURL: location.hostname === "localhost" ? "http://localhost:6565/api/v1" : "/api",
+    baseURL: "http://localhost:6565/api/v1",
     withCredentials: true
 });
