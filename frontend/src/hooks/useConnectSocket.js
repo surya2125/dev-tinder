c6e66ad910abb687b0b71129fb794250032cb0df
Sharadindu Das <sharadindudas774@gmail.com>
2025-04-04 23:14:38 +0530

    fix: Fixed the notification sound bug

--- frontend/src/hooks/useConnectSocket.js
+++ frontend/src/hooks/useConnectSocket.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router";
@@ -11,31 +11,50 @@ const useConnectSocket = (userId) => {
     const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
 
+    const recentlyPlayed = useRef(false);
+
     useEffect(() => {
-        const socket = io(import.meta.env.VITE_BACKEND_URL, {
+        if (socket) return;
+
+        const newSocket = io(import.meta.env.VITE_BACKEND_URL, {
             withCredentials: true
         });
-        setSocket(socket);
+        setSocket(newSocket);
+
+        return () => {
+            newSocket.close();
+            setSocket(null);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (!socket || !user?._id || !userId) return;
 
         socket.emit("joinChat", { name: user?.name, senderId: user?._id, receiverId: userId });
 
-        socket.on("error", (err) => {
-            toast.error(err);
+        socket.on("error", (errMessage) => {
+            toast.error(errMessage);
             setIsLoading(true);
             setTimeout(() => navigate("/feed", { replace: true }), 1000);
         });
 
         socket.on("messageReceived", (newMessage) => {
-            const sound = new Audio(notificationSound);
-            sound.play();
             updateMessages(newMessage);
+            if (!recentlyPlayed.current) {
+                const sound = new Audio(notificationSound);
+                sound.play();
+                recentlyPlayed.current = true;
+                setTimeout(() => {
+                    recentlyPlayed.current = false;
+                }, 300);
+            }
         });
 
         return () => {
-            socket.close();
-            setSocket(null);
+            socket.off("error");
+            socket.off("messageReceived");
         };
-    }, [navigate, updateMessages, user?._id, user?.name, userId]);
+    }, [socket, user?._id, user?.name, userId, navigate, updateMessages]);
 
     return { socket, isLoading };
 };
